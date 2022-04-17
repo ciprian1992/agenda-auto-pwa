a9a5cb85899946869a050639e53e8d37c5bf9fad
unknown <ciprian.grindeanu@gmail.com>
2022-04-17 19:54:15 +0300

    fix

--- agenda-auto-pwa/src/app/modules/consumables/consumables.module.ts
+++ agenda-auto-pwa/src/app/modules/consumables/consumables.module.ts
@@ -7,8 +7,6 @@ import { IonicModule } from '@ionic/angular';
 import { ConsumablesPageRoutingModule } from './consumables-routing.module';
 
 import { ConsumablesPage } from './consumables.page';
-import { LuxonDatePipe } from 'src/app/services/ui/luxon-date.pipe';
-import { ConsumableTypePipe } from 'src/app/services/ui/type-token.pipe';
 import { TranslocoModule } from '@ngneat/transloco';
 import { SharedModule } from 'src/app/services/ui/shared.module';
 
--- agenda-auto-pwa/src/app/services/ui/shared.module.ts
+++ agenda-auto-pwa/src/app/services/ui/shared.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { TranslocoModule, TranslocoPipe } from '@ngneat/transloco';
 import { CalendarEventsService } from './calendar-events.service';
 import { LuxonDatePipe } from './luxon-date.pipe';
 import { ConsumableTypePipe, DocumentTypePipe } from './type-token.pipe';
@@ -6,6 +7,6 @@ import { ConsumableTypePipe, DocumentTypePipe } from './type-token.pipe';
 @NgModule({
   exports: [LuxonDatePipe, DocumentTypePipe, ConsumableTypePipe],
   declarations: [LuxonDatePipe, DocumentTypePipe, ConsumableTypePipe],
-  providers: [CalendarEventsService],
+  providers: [CalendarEventsService, TranslocoPipe],
 })
 export class SharedModule {}
